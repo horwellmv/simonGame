502982ef0c9ccfdc1b4a8b45af997e02587cee44
horwellmv <horwellmv@gmail.com>
2020-10-24 22:04:10 -0300

    Archivo Program.cs c# con el codigo inicial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace simonGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a SimonGame");
+            Console.WriteLine("Nuestros elementos serán vocales.");
+
+        }
+    }
+}
