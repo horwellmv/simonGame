71c9019fdecf54383f2326e855a8f74c3ad1921d
horwellmv <horwellmv@gmail.com>
2020-10-25 01:20:07 -0300

    Definimos variables, listas y colas + funcion para mostrar cola en pantalla

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 
 namespace simonGame
 {
@@ -10,6 +10,39 @@ namespace simonGame
             Console.WriteLine("Bienvenido a SimonGame");
             Console.WriteLine("Nuestros elementos serán vocales.");
 
+            // Definimos variables. 
+            int index;
+            string[] listElements = new string[] { "a", "e", "i", "o", "u" }; // Elementos para el juego
+            string[] listUser = new string[] { }; // Lista para los input del usuario.
+            Random aleatory = new Random();
+
+            Console.WriteLine(listElements.Length);
+
+
+
+            index = aleatory.Next(0, listElements.Length);
+
+            Console.WriteLine("Vocal es: {0}", listElements[index]);
+
+            Queue colaMachine = new Queue();
+            colaMachine.Enqueue(listElements[index]);
+            index = aleatory.Next(0, listElements.Length);
+            colaMachine.Enqueue(listElements[index]);
+            PrintQueue(colaMachine);
+
+
+
+
+
+
+        }
+
+
+        public static void PrintQueue(IEnumerable myQueue)
+        {
+            foreach (Object element in myQueue)
+                Console.Write(" - {0}", element);
+            Console.WriteLine();
         }
     }
 }
